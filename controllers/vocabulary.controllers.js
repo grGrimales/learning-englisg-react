cf274c47b9e7404391f8454c1f1184d390c01641
Carlos Guzman <carlos.guzman3.contractor@bbva.com>
2022-03-22 16:09:06 -0500

    order de metodo get

--- controllers/vocabulary.controllers.js
+++ controllers/vocabulary.controllers.js
@@ -1,5 +1,5 @@
 const { response } = require('express');
-const { randomOrder, orderByLeastPlayed } = require('../helpers/helpers');
+const { randomOrder, orderByLeastPlayed, orderByLeastHits } = require('../helpers/helpers');
 
 const { Vocabulary } = require('../models/');
 
