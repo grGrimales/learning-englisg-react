a38d801fad7e08e81705d195e503bbbb5d8e9ac4
GrGrimales <grojasgrimales@gmail.com>
2022-04-22 10:06:25 -0500

    gh pages añadido

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Login } from "../components/auth/Login";
